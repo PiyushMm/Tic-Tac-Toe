7e54b5a23b863e938e40da77541d2c456cff2046
Prashant Chaturvedi <42546403+vediprashant@users.noreply.github.com>
2022-09-05 23:40:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,11 +14,12 @@ let boxtext = document.getElementsByClassName("boxtext");
     let wins = [
     [0,1,2,5,5,0],
     [3,4,5,5,15,0],
-    [6,7,8,5,15,0],
+    [6,7,8,5,25,0],
     [0,3,6,-5,15,90],
     [1,4,7,5,15,90],
     [0,4,8,5,15,45],
     [2,4,6,5,15,135],
+    [2,5,8,10,15,90],
 ]
  wins.forEach(e =>{
     if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[1]].innerText === boxtext[e[2]].innerText)&& (boxtext[e[0]].innerText !=='')){
